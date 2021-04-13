86fb239a8c99a4645d83a768d191f8445ff84872
Krys Madel <madel3@siebl-0224-40.ews.illinois.edu>
2021-04-13 16:53:11 -0500

    Added graph.h functions

--- Graph.h
+++ Graph.h
@@ -2,5 +2,10 @@
 
 class Graph {
     public:
+        void insertVertex(Node);
+        Node removeVertex(Node);
+        void insertEdge(Node, Node, K key);
+        void removeEdge(Node, Node);
     private:
+        
 };
