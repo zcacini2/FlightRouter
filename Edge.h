ff5ec65ef4aee127c2d8d6f684e1eec3168c8802
Zach Cacini <zcacini2@siebl-0224-38.ews.illinois.edu>
2021-04-13 17:14:14 -0500

    Begin Edge Class

--- Edge.h
+++ Edge.h
@@ -1,1 +1,18 @@
 #pragma once
+
+/**
+ * The Edge class represents a connection between two airports in the graph linked-memory structure. 
+ */
+class Edge {
+
+    public:
+        /**
+         * Put Edge public members here.
+         */
+
+    private:
+        /**
+         * Put Node private members here.
+         */
+
+}
