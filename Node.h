9864552903cf0b35bfe7c397887808ee81cdc23d
Zach Cacini <zcacini2@siebl-0224-38.ews.illinois.edu>
2021-04-13 16:52:17 -0500

    Node additions

--- Node.h
+++ Node.h
@@ -1,1 +1,32 @@
 #pragma once
+
+
+/**
+ * The Node class represents an Airport in the graph linked-memory structure. 
+ */
+class Node {
+
+    public:
+        /**
+         * Put Node public members here.
+         */
+
+    private:
+        /**
+         * Put Node private members here.
+         */
+
+        //The beginning/necessary components
+        string airportCode;
+        double latitude;
+        double longitude;
+
+        /*
+        Necessary for advanced input methods
+        string name;
+        string city;
+        string country;
+        */
+        
+
+};
