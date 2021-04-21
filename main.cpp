939b9fd702540a63c0e29d5f355dd1a006ec065c
Krys Madel <madel3@siebl-0224-34.ews.illinois.edu>
2021-04-21 15:09:28 -0500

    added test for entire dataset in main

--- main.cpp
+++ main.cpp
@@ -5,13 +5,14 @@
 #include <iostream>
 
 int main() {
-    //const int& testint = 5;
     const std::string& filename1 = "tests/routesDataSmall.txt";
     Graph graph1(filename1);
-    //Node testNode = Node(1, -6.081689834590001, 145.391998291);
-    //testNode.addNeighbor()
     BFS test(graph1, graph1.getFirstNode());
     test.traverse();
 
+    const std::string& filename2 = "dataset/routes.txt";
+    Graph graph2(filename2);
+    BFS bfs2(graph2, graph2.getFirstNode());
+    bfs2.traverse();
     return 0;
 }
