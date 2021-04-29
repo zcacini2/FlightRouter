263dfe036630ee8fc098310b14f09d686a5e4c6e
Jung, Giwon <gjung10@illinois.edu>
2021-04-30 07:23:34 +0900

    Update dijkstra.h

--- dijkstra.h
+++ dijkstra.h
@@ -18,16 +18,8 @@ class dijkstra {
       /* dijkstra constructor. The constructor takes graph and two nodes:
       first node is starting point(source) and second node is ending point (destination)
       */
-<<<<<<< HEAD
-
-
-    vector<Node> dijkstra(Graph * graph, Node *start, Node * end);
+    vector<Node> shortestPath(Graph * graph, Node *start, Node * end);
       /* dijkstra function. this function takes graph and two Nodes
-=======
-      
-    vector<Node> dijkstra(Graph * graph, Node *start, Node * end);
-      /* dijkstra function. this function takes graph and two Nodes 
->>>>>>> 30d0912ea20226fba177a31a3b931cbb83693ca6
       and return shortest distance from starting point to the end.
       */
 
