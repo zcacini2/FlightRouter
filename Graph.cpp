e776f192405e57623d662ae0baf40cc8d570fbe5
Jung, Giwon <gjung10@illinois.edu>
2021-04-30 11:55:47 +0900

    Updated dijkstra function in graph

--- Graph.cpp
+++ Graph.cpp
@@ -4,7 +4,13 @@
 #include "Edge.h"
 #include "readFromFile.hpp"
 #include "Airports.h"
+#include <queue>
+#include <set>
+#include <unordered_map>
 
+using namespace std;
+
+typedef pair<double, int> distanceToNode;
 
 Graph::Graph() { }
 
@@ -18,29 +24,29 @@ Graph::~Graph() {
     }
   }
 
-  
+
   for (unsigned i = 0; i < edges_.size(); i++) {
     //delete edges_[i];
   }
   */
-  
+
 }
 
 /**
- * Graph constructor. The constructor will take in the routes file. The routes file is a CSV containing the 
+ * Graph constructor. The constructor will take in the routes file. The routes file is a CSV containing the
  * following elements in order: Airline, Airline ID, Source airport, Source Airport (OpenFlights ID), Destination
  * Airport, Destination Airport (OpenFlights ID), CodeShare, Stops, and Equipment. The only values that are of
  * importance to us are the Source Airport (OpenFlights ID) and the Destination Airport (OpenFlights ID). Stops are
- * all 0 (direct flight) for the entire file. 
+ * all 0 (direct flight) for the entire file.
  *
- * The goal of the constructor is to convert the CSV into a vector. Then, using this vector, we want to find the 
+ * The goal of the constructor is to convert the CSV into a vector. Then, using this vector, we want to find the
  * two values of interest. With the two values of interest, we must build their nodes if they dont exist. Data
  * required for nodes are airport code, latitude, and longitude. These two values will be matched using the Airports
- * coder/decoder. The OpenFlights ID ends up  being simply the index of the vector for which it's information is 
- * stored. 
- * 
- * So, if the nodes and edge exist, we do nothing for that line. If the nodes exist but not the edge, we must add 
- * an edge. If neither the node or the edge exist, we must create the proper nodes (reference the Airport 
+ * coder/decoder. The OpenFlights ID ends up  being simply the index of the vector for which it's information is
+ * stored.
+ *
+ * So, if the nodes and edge exist, we do nothing for that line. If the nodes exist but not the edge, we must add
+ * an edge. If neither the node or the edge exist, we must create the proper nodes (reference the Airport
  * coder/decoder) and add the edge to the adjacency list.
  *
  * @param filename name of the file to create the graph from
@@ -58,7 +64,7 @@ Graph::Graph(const std::string & routesFile, const std::string & airportsFile) {
 
   vector<string> line;  //create line vector
   stringstream s_stream(str); //create string stream from the string
-  int sourceID, destID; 
+  int sourceID, destID;
   bool areBothValid;
 
   while(s_stream.good()) {
@@ -72,10 +78,10 @@ Graph::Graph(const std::string & routesFile, const std::string & airportsFile) {
       substr.erase(std::remove( substr.begin(), substr.end(), '\"' ), substr.end()); //cleans quotations
       line.push_back(substr);
     }
-    
+
     sourceID = stoi(line[3]); //OpenFlights ID of the source airport
     destID = stoi(line[5]); //OpenFlights ID of the destination airport
-    
+
     /*
     // Tries to convert sourceID to double
     try {
@@ -132,7 +138,7 @@ Graph::Graph(const std::string & routesFile, const std::string & airportsFile) {
         std::cerr << "Invalid sourceLat on line " << edges_.size() << " with value " << sourceID << std::endl;
         break;
       }
-      
+
       // Tries to get source lng
       try {
         sourceLng = airports.longitude(sourceID);
@@ -141,7 +147,7 @@ Graph::Graph(const std::string & routesFile, const std::string & airportsFile) {
         break;
       }
       */
-      
+
 
       Node* _source = new Node(sourceID, sourceLat, sourceLng);
       source = _source;
@@ -171,7 +177,7 @@ Graph::Graph(const std::string & routesFile, const std::string & airportsFile) {
         std::cerr << "Invalid destLat on line " << edges_.size() << " with value " << destID << std::endl;
         break;
       }
-      
+
       // Tries to get dest lng
       try {
         destLng = airports.longitude(destID);
@@ -212,10 +218,10 @@ Graph::Graph(const std::string & routesFile, const std::string & airportsFile) {
   /*
   Functionality to add:
     - Handling null (\N) values (Delete from file?)
-    - Parsing for invalid values 
+    - Parsing for invalid values
     - Check airport for valid airport
   */
-  
+
 }
 
 /**
@@ -234,6 +240,112 @@ vector<Edge> Graph::getEdges() {
 
 Node* Graph::getFirstNode() {
   return (nodes_.at(1));
+}
+
+double Graph::getDistance(int start, int end) {
+  Node * start_node = new Node();
+  Node * end_node = new Node();
+  for (Node * node: nodes_){
+    if (node->airportCode() == start) {
+      start_node = node;
+    }
+    if (node->airportCode() == end){
+      end_node = node;
+    }
+  }
+
+  if (start_node->areNeighbors(end_node)) {
+    return start_node->distance(end_node);
+  }
+
+  return 9999999;
+}
+vector<int> Graph::shortestPath(int start, int end){
+    //struct for comparison function of priority queue
+    struct CompareDistance{
+      bool operator()(std::pair<int, double> const & a, std::pair<int, double> const & b){
+        //return true if a is bigger than b
+        // ensures lower distances are prioritized in a queue
+        return a.second> b.second;
+      }
+    };
+
+
+    //size = number of all nodes(vertices) in graph
+    int size = nodes_.size();
+
+    std::unordered_map<int, double> distances_; // retrieve distance values for each node
+    std::unordered_map<int, int> routes_;    // initialize a route that records node->its previous node
+
+    //initialize a priority queue of node-distance pair
+    typedef std::priority_queue<std::pair<int, double>, vector<std::pair<int, double>>, CompareDistance> pq;
+    pq q;
+    std::set<int> visited; //initialize set 'visited' to chech whether this nodes are visited
+
+    //getting nodes from airportcode
+    Node * startNode = new Node();
+    Node * endNode = new Node();
+    for (Node * node: nodes_){
+      if (node->airportCode() == start) {
+        startNode = node;
+      }
+      if (node->airportCode() == end){
+        endNode = node;
+      }
+    }
+
+    //check if there's adjacent nodes from starting node
+    list<Node*> adjacent = startNode->neighbors();
+    if(adjacent.empty()){
+      return vector<int>(); //return empty vector if there's no adjacent node
+    }
+
+    //initialize edges in distance map to be large enough
+    for(auto v: ) {
+      distances_[v] = 9999999;
+    }
+
+    //set distances of starting point to be zero
+    distances_[start] = 0;
+    q.push(pair<int,double>(start, 0));
+
+    //loop until we reach destination
+    while (q.top().first != end && !q.empty()){
+      std::pair<int, double> curr = q.top(); //get the next pair from priority_queue
+      int curr_node = curr.first;
+      q.pop();
+
+      //mark current node as visited
+      routes_.insert(curr_node);
+
+      list<int> neighbors = curr_node.neighbors_codes(); // get all adjacent nodes
+
+      for (int neighbor : neighbors){
+        if (visited.find(neighbor) == visited.end()){ // if there's no node neighbor in visited
+          double dist = distances_[curr_node] + getDistance(nodes, neighbor);
+          if (dist <= distances_[neighbor]) {
+            distances_[neighbor] = dist;
+            routes_[neighbor] = curr_vertex;
+            q.push(std::pair<int, double>(neighbor, dist));
+          }
+        }
+      }
+    }
+
+    if(routes_.find(end) == routes_.end()){ // if no path exists
+      return vector<int>();                          //return empty vector
+    }
+
+    //backtracing the path from routes_
+    vector<int> path;
+    int curr = end;
+    while (curr != start) {
+      path.push_back(curr);
+      curr = routes_[curr];
+    }
+    path.push_back(start);
+    std::reverse(path.begin(), path.end());
+    return path;
 }
 
 //void Graph::print() { }
@@ -292,7 +404,7 @@ bool Graph::areAdjacent(Node node1, Node node2){
 
 /**
  * This method will insert a node into the nodes_ array maintained by the graph.
- * @param toAdd the Node to add to the nodes_ array 
+ * @param toAdd the Node to add to the nodes_ array
  */
 /*void Graph::insertNode(Node toAdd) {
 
@@ -302,12 +414,12 @@ bool Graph::areAdjacent(Node node1, Node node2){
 
 /**
  * This method will remove a node from the nodes_ array maintained by the graph. Also, must remove all edges associated with that node.
- * To remove the edges, parse throgh all edges maintained by the node, go to 
+ * To remove the edges, parse throgh all edges maintained by the node, go to
  */
 /*void Graph::removeNode(Node toRemove) {
-  
+
   nodes_.emplace(nodes_.begin() + toRemove.airportCode(), Node());
-  
-  
-  
+
+
+
 }*/
